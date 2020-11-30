e86835fda897682225271dcca2b7a76e1d819135
Abdullah <abdullah@abdullah.today>
2020-12-01 00:15:31 +0500

    adding proxy support
    server now needs to use proxies as its not accessible from Pakistan

--- data.py
+++ data.py
@@ -21,8 +21,19 @@ import requests
 from bs4 import BeautifulSoup
 
 
+proxies = {
+        'https': 'http://103.144.201.112:8080',
+        'https': 'http://203.81.91.80:8080',
+        'https': 'http://152.26.66.140:3128',
+        'https': 'http://51.79.79.111:3128',
+        'https': 'http://168.169.96.2:8080',
+        'https': 'http://64.235.204.107:8080',
+        'https': 'http://142.44.243.113:3128',
+        'https': 'http://136.243.133.76:3128'
+        }
+
 def get_data(number):
-    r = requests.post('https://allnumber.cc/get.php', data={'number': number})
+    r = requests.post('https://allnumber.cc/get.php', data={'number': number}, proxies=proxies)
     soup = BeautifulSoup(r.content, 'html.parser')
     for data in soup.find_all('td'):
         print(data.get_text())
